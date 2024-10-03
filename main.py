aaf3d5ee5b9d29e3960ccdeaa9a5239953e3d195
Joel <joeladamsch@gmail.com>
2024-10-02 23:29:10 -0500

    sudo code outline

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-import motor_control
+import ros_send
+#import statements
 
-motor_control.send_motor_positions([2,5])
 
+#define callback functions for ros_receive
 
+#define function for calcutating IK that gets called in the main loop
+
+motor_angles=[0,0,0,0,0,0]
+
+IK_target_pos=[0,0,0]
+
+# def calculate IK
+# def calculate direct motor control
+
+while running:# main loop:
+    #handle pygame events (such as closing command)
+
+    #draw GUI
+        #graph data
+        #displaying logs
+        #displaying motor angles and stuff
+    
+    if IK:
+        calculate_IK()
+    else:
+        direct_motor_control()
+    clamp_andgles()
---
+++ ros_receive.py
@@ -0,0 +1,11 @@
+
+# define functions and then start rclpy.spin in new thread
+
+
+# catch messages from Lebre
+# determind type of data
+
+#data types and what to do with each:
+    # sensor data: logged and send to main GUI
+    # servo motor positions: send to main
+    # log messages: send to main
